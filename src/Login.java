1074e10a986770b674072f01c1b9e1eb8e38dd83
div <rasaniadivya1@gmail.com>
2026-04-19 14:11:32 -0400

    changed layout to add padding, and use custom fonts

--- src/Login.java
+++ src/Login.java
@@ -2,14 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
 /**
  * A login screen for the PhoneBook application.
  *
- * @author Divya Rasania
+ * @author Divya Rasania, David Komiyama
  */
 public class Login extends JFrame implements ActionListener {
-
     private static final String DEFAULT_USERNAME = "admin";
     private static final String DEFAULT_PASSWORD = "admin";
 
@@ -18,7 +20,7 @@ public class Login extends JFrame implements ActionListener {
     private JPasswordField passwordTextField;
     private JPanel loginPanel, headingPanel, iconPanel;
     private JButton loginButton, cancelButton;
-    private Font font1, font2, font;
+    private Font headingFont30, headingFont35, regularFont;
 
     public Login() {
         initializeComponents();
@@ -31,7 +33,7 @@ public class Login extends JFrame implements ActionListener {
         welcomeLabel2 = new JLabel("Please login to proceed");
         usernameLabel = new JLabel("Username");
         passwordLabel = new JLabel("Password");
-        iconLabel = new JLabel(new ImageIcon(ClassLoader.getSystemResource("icons/user.png")));
+        iconLabel = new JLabel(new ImageIcon(ClassLoader.getSystemResource("resources/icons/user.png")));
         usernameTextField = new JTextField();
         passwordTextField = new JPasswordField();
         loginPanel = new JPanel();
@@ -39,24 +41,53 @@ public class Login extends JFrame implements ActionListener {
         iconPanel = new JPanel();
         loginButton = new JButton("Login");
         cancelButton = new JButton("Cancel & Exit");
-        font = new Font("Calibri", Font.BOLD, 20);
+
+        List<URL> customFonts = List.of(
+                ClassLoader.getSystemResource("resources/fonts/MirandaSans/MirandaSans-Bold.ttf"),
+                ClassLoader.getSystemResource("resources/fonts/MirandaSans/MirandaSans-Regular.ttf")
+        );
+        loadCustomFonts(customFonts);
+
+        headingFont30 = new Font("Miranda Sans", Font.BOLD, 30);
+        headingFont35 = new Font("Miranda Sans", Font.BOLD, 35);
+        regularFont = new Font("Miranda Sans", Font.PLAIN, 20);
+    }
+
+    private void loadCustomFonts(List<URL> fontFiles) {
+        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        List<String> availableFonts = List.of(ge.getAvailableFontFamilyNames());
+
+        for (URL url : fontFiles) {
+            if (url != null) {
+                try {
+                    Font f = Font.createFont(Font.TRUETYPE_FONT, url.openStream());
+                    if (!availableFonts.contains(f.getFontName()))
+                        ge.registerFont(f);
+                } catch (FontFormatException | IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+            }
+        }
     }
 
     private void layoutComponents() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
-        setIconImage(new ImageIcon(ClassLoader.getSystemResource("icons/login.png")).getImage());
+        setIconImage(new ImageIcon(ClassLoader.getSystemResource("resources/icons/login.png")).getImage());
         setTitle("Login");
         setLayout(new BorderLayout(10, 20));
 
-        welcomeLabel.setFont(new Font("Calibri", Font.BOLD, 35));
-        welcomeLabel2.setFont(new Font("Calibri", Font.BOLD, 30));
-        usernameLabel.setFont(font);
-        usernameTextField.setFont(font);
-        passwordLabel.setFont(font);
-        passwordTextField.setFont(font);
-        loginButton.setFont(font);
-        cancelButton.setFont(font);
+        welcomeLabel.setFont(headingFont35);
+        welcomeLabel2.setFont(headingFont30);
+        usernameLabel.setFont(regularFont);
+        usernameTextField.setFont(regularFont);
+        passwordLabel.setFont(regularFont);
+        passwordTextField.setFont(regularFont);
+        loginButton.setFont(regularFont);
+        cancelButton.setFont(regularFont);
+
+        loginButton.setPreferredSize(new Dimension(200, 40));
+        cancelButton.setPreferredSize(new Dimension(200, 40));
 
         // Heading panel
         headingPanel.setLayout(new GridLayout(2, 1, 10, 10));
@@ -64,7 +95,6 @@ public class Login extends JFrame implements ActionListener {
         headingPanel.add(welcomeLabel2);
 
         // Icon panel
-        iconPanel.setLayout(new GridLayout(1, 1, 10, 10));
         iconPanel.add(iconLabel);
 
         // Login panel
@@ -77,6 +107,7 @@ public class Login extends JFrame implements ActionListener {
         loginPanel.add(cancelButton);
 
         // Add panels to the main frame
+        ((JPanel) getContentPane()).setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20)); // padding for all sides so text dont stick to edges
         add(headingPanel, BorderLayout.NORTH);
         add(iconPanel, BorderLayout.EAST);
         add(loginPanel, BorderLayout.CENTER);
@@ -86,11 +117,12 @@ public class Login extends JFrame implements ActionListener {
     private void addListeners() {
         loginButton.addActionListener(this);
         cancelButton.addActionListener(this);
+        passwordTextField.addActionListener(this);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == loginButton) {
+        if (e.getSource() == loginButton || e.getSource() == passwordTextField) {
             // Get the username and password from the text fields
             String username = usernameTextField.getText();
             String password = passwordTextField.getText();
